b8ff98618e46945220fa00e1e8e49e1ef925d52c
JerryJie <e25254@gmail.com>
2025-05-06 22:56:37 +0800

    feat( Divider ) : `Divider` completed ( #main )

---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,19 @@
+interface DividerProps {
+  className?: string;
+  orientation?: "horizontal" | "vertical";
+}
+
+export default function Divider({
+  className = "",
+  orientation = "horizontal",
+}: DividerProps) {
+  const orientationClassObj = {
+    horizontal: "w-full border-t-[1px] border-l-0",
+    vertical: "h-full border-l-[1px] border-t-0",
+  };
+  return (
+    <hr
+      className={`border-gray-500 opacity-10 ${orientationClassObj[orientation]} ${className}`}
+    />
+  );
+}
--- src/containers/OrderBook/index.tsx
+++ src/containers/OrderBook/index.tsx
@@ -1,9 +1,16 @@
+import Divider from "@/components/Divider";
 import Stack from "@/components/Stack";
 import Quotes from "@/modules/Quotes";
 export default function OrderBook() {
   return (
     <Stack>
-      <Quotes />
+      <Stack className="py-4 px-hor-container">
+        <h1 className="text-md font-bold">Order Book</h1>
+      </Stack>
+      <Divider />
+      <Stack className="px-hor-container">
+        <Quotes />
+      </Stack>
     </Stack>
   );
 }
